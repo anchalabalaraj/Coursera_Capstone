1488824f0c744faac8346b2e2250d427e1c6ac62
anchalabalaraj <57788979+anchalabalaraj@users.noreply.github.com>
2021-06-05 21:22:00 +0530

    Capstone Project Notebook of Week 1
    This is the first notebook of Applied Data Science Capstone Course for Peer-graded Assignment: Capstone Project Notebook of Week 1

---
+++ week1_assignment.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This is the first notebook of Applied Data Science Capstone Course for Peer-graded Assignment: Capstone Project Notebook of Week 1
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
